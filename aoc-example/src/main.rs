483794e79e835ea75545e410eeda7963825b7b07
Omay238 <238maculo238@gmail.com>
2025-11-05 15:07:16 -0700

    initial library and base code

---
+++ aoc-example/src/main.rs
@@ -0,0 +1,12 @@
+use aoc_lib;
+
+fn main() {
+    let mut answers = (0, 0);
+
+    let input = aoc_lib::get_input();
+
+    aoc_lib::print_day(1);
+    println!("{}", answers.0);
+    aoc_lib::print_day(2);
+    println!("{}", answers.1);
+}
---
+++ aoc-lib/src/lib.rs
@@ -0,0 +1,26 @@
+use ureq;
+
+fn get_name() -> (String, String) {
+    let filename = std::env::current_exe().unwrap();
+    let mut date_string = filename.to_str().unwrap().split("aoc-").last().unwrap().split("-");
+    let year = date_string.next().unwrap();
+    let day = date_string.next().unwrap();
+    (String::from(year), String::from(day))
+}
+
+pub fn print_day(part: u8) {
+    let (year, day) = get_name();
+
+    println!("{} day {} part {}", year, day, part)
+}
+
+pub fn get_input() -> String {
+    let (year, day) = get_name();
+    ureq::get(format!("https://adventofcode.com/{}/day/{}/input", year, day).as_str())
+        .header("Cookie", format!("session={}", std::env::var("SESSION").unwrap()))
+        .call()
+        .unwrap()
+        .body_mut()
+        .read_to_string()
+        .unwrap()
+}
