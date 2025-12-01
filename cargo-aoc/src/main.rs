1d3504b325d40251072f75196c10b00afe262305
Omay238 <238maculo238@gmail.com>
2025-12-01 02:12:29 -0700

    fmt

--- cargo-aoc/src/main.rs
+++ cargo-aoc/src/main.rs
@@ -14,11 +14,17 @@ fn get_default_help() -> String {
     help = format!("{help}{}", "    new".bold().bright_cyan());
     help = format!("{help}{}", "    Create an aoc project in a new directory\n");
     help = format!("{help}{}", "    init".bold().bright_cyan());
-    help = format!("{help}{}", "   Create an aoc project in the current directory\n");
+    help = format!(
+        "{help}{}",
+        "   Create an aoc project in the current directory\n"
+    );
     help = format!("{help}{}", "    year".bold().bright_cyan());
     help = format!("{help}{}", "   Add a new year to the current aoc project\n");
     help = format!("{help}{}", "    day".bold().bright_cyan());
-    help = format!("{help}{}", "    Add a new day to a year in the current aoc project\n\n");
+    help = format!(
+        "{help}{}",
+        "    Add a new day to a year in the current aoc project\n\n"
+    );
     help = format!("{help}{}", "See '");
     help = format!("{help}{}", "aoc help ".bold().bright_cyan());
     help = format!("{help}{}", "<command>".cyan());
@@ -53,18 +59,27 @@ fn main() {
                     help = format!("{help}{}", "Arguments:\n".bold().bright_green());
                     help = format!("{help}{}", "    <PATH>".cyan());
                 } else if cur_arg == "init" {
-                    help = format!("{help}{}", "Create a new AOC project in the current directory\n\n");
+                    help = format!(
+                        "{help}{}",
+                        "Create a new AOC project in the current directory\n\n"
+                    );
                     help = format!("{help}{}", "Usage: ".bold().bright_green());
                     help = format!("{help}{}", "cargo aoc init".bold().bright_cyan());
                 } else if cur_arg == "year" {
-                    help = format!("{help}{}", "Add a new year to the AOC project in the current directory\n\n");
+                    help = format!(
+                        "{help}{}",
+                        "Add a new year to the AOC project in the current directory\n\n"
+                    );
                     help = format!("{help}{}", "Usage: ".bold().bright_green());
                     help = format!("{help}{}", "cargo aoc year ".bold().bright_cyan());
                     help = format!("{help}{}", "<YEAR>\n\n".cyan());
                     help = format!("{help}{}", "Arguments:\n".bold().bright_green());
                     help = format!("{help}{}", "    <YEAR>".cyan());
                 } else if cur_arg == "day" {
-                    help = format!("{help}{}", "Add a new day to the specified year in the AOC project in the current directory\n\n");
+                    help = format!(
+                        "{help}{}",
+                        "Add a new day to the specified year in the AOC project in the current directory\n\n"
+                    );
                     help = format!("{help}{}", "Usage: ".bold().bright_green());
                     help = format!("{help}{}", "cargo aoc day ".bold().bright_cyan());
                     help = format!("{help}{}", "<YEAR> <DAY>\n\n".cyan());
